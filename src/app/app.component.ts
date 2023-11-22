05ff6b9b569c85270c5ec85afc7b26a9f2710646
Daniel Tovar <91617620+Danny112T@users.noreply.github.com>
2023-11-22 08:06:58 -0600

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CBDF_DMTP_09';
+  title = 'Ejercicio 09 - Interpolación';
+  nombre = "Daniel Tovar";
+  edad = 21;
+  semestre = "Septimo Semestre";
+  calificaciones = [9.5, 10, 7, 8, 9, 8, 8.9, 10, 9.5, 9.8]
+  promedio = this.calcularPromedio();
+  estado = this.estadoAlumno();
+
+  calcularPromedio() {
+    let suma = 0;
+    for (let i = 0; i < this.calificaciones.length; i++) {
+      suma += this.calificaciones[i];
+    }
+    return suma / this.calificaciones.length;
+  }
+
+  estadoAlumno() {
+    if (this.promedio >= 6) {
+      return "Aprobado";
+    } else {
+      return "Reprobado";
+    }
+  }
 }
